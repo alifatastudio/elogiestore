45d887c36e9539f882d53d1600261892f58ad92b
Fikri Ali Fata <me.alifata@gmail.com>
2020-10-24 22:56:26 +0700

    Fix SQL

--- library/Database.js
+++ library/Database.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : process.env.DB_HOST,
-  user     : process.env.DB_USER,
-  password : process.env.DB_PASSWORD,
-  database : process.env.DB_DATABASE
+	socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+ user     : process.env.SQL_USER,
+ password : process.env.SQL_PASSWORD,
+ database : process.env.SQL_DATABASE
 });
  
 function testConnection(){
