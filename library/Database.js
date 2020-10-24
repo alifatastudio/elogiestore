22ad61876f848f3d63cbb2bae8c9c5eeed97ebea
Fikri Ali Fata <me.alifata@gmail.com>
2020-10-24 23:42:21 +0700

    Come on

--- library/Database.js
+++ library/Database.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
 	socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+ host     : process.env.SQL_HOST,
  user     : process.env.SQL_USER,
  password : process.env.SQL_PASSWORD,
  database : process.env.SQL_DATABASE
